6a6ed4bebc2542f287e3b7e6f7589c72a39bc209
Nathan Chere <git@nathanchere.com.au>
2017-12-01 14:03:14 +0100

    python tests setup

--- README.md
+++ README.md
@@ -2,4 +2,13 @@
 
 C# solutions assume .Net Core 2.0
 Elixir solutions assume Elixr 1.5
-Python solutions assume Python 3.6
+
+## Python
+
+Solutions assume Python 3.6.
+
+Run `pip install pytest` to enable unit test support
+
+Run solutions with e.g. `python day01.py`
+
+Run tests with `py.test day01.py`
--- day01/python/day01.py
+++ day01/python/day01.py
@@ -1,26 +1,28 @@
 # Advent of Code 2017
 # Day 01: "Inverse Captcha" (Python)
 
-import pytest
 import common
 
 def solve(input):
     return 9
 
-def test_example_1():
-    assert solve('1122') == 9
+class Tests():
+    import pytest
+    
+    def test_example_1(x):
+        assert solve('1122') == 9
 
-def test_example_2():
-    assert solve('1111') == 4
+    def test_example_2(x):
+        assert solve('1111') == 4
 
-def test_example_3():
-    assert solve('1234') == 0
+    def test_example_3(x):
+        assert solve('1234') == 0
 
-def test_example_4():
-    assert solve('91212129') == 9
+    def test_example_4(x):
+        assert solve('91212129') == 9
 
-def test_solve():
-    assert solve(get_input('day01'))
+    def test_solve(x):
+        assert solve(get_input('day01'))
 
 if __name__ == "__main__":
     print(solve(get_input('day01')))
