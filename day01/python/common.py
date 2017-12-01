a10a07ab8236b8d14a35a51f78ed3743e665419b
Nathan Chere <git@nathanchere.com.au>
2017-12-01 13:59:01 +0100

    Python placeholder

---
+++ day01/python/common.py
@@ -0,0 +1,5 @@
+import os
+
+def get_input(day):
+    with open(f'../{day}.input','r') as f:
+        return f.read()
--- day01/python/day01.py
+++ day01/python/day01.py
@@ -1,11 +1,26 @@
 # Advent of Code 2017
 # Day 01: "Inverse Captcha" (Python)
 
-import os
+import pytest
+import common
 
-def get_input(day):
-    with open(f'../{day}.input','r') as f:
-        return f.read()
+def solve(input):
+    return 9
+
+def test_example_1():
+    assert solve('1122') == 9
+
+def test_example_2():
+    assert solve('1111') == 4
+
+def test_example_3():
+    assert solve('1234') == 0
+
+def test_example_4():
+    assert solve('91212129') == 9
+
+def test_solve():
+    assert solve(get_input('day01'))
 
 if __name__ == "__main__":
-    print(get_input('day01'))
+    print(solve(get_input('day01')))
