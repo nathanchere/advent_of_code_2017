e050a80a5affdf0bc8d8e6c4dfc621d774d2851b
Nathan Chere <git@nathanchere.com.au>
2017-12-01 17:04:39 +0100

    C# placeholder

---
+++ solutions/csharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace csharp
+{
+    class Program
+    {
+        static string GetInput(string day)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
