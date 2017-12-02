e1a5cb847f55ee73205d5bf0605e91c741d21521
Nathan Chere <git@nathanchere.com.au>
2017-12-02 06:11:52 +0100

    Pythong day 2 part 1

---
+++ solutions/python/day02a.py
@@ -0,0 +1,36 @@
+# Advent of Code 2017
+# Day 02: "Checksum Corruption" Part 1 (Python)
+
+from common import *
+import re
+
+def solve(input):
+    result = 0
+    for line in input.splitlines():
+        result += get_row_difference(line)
+    return result
+
+def get_row_difference(input):
+    minValue=999999999999
+    maxValue=0
+    regex = re.compile("[^\t]+")
+    for value in regex.findall(input):
+        numberValue = int(value)
+        if numberValue > maxValue:
+            maxValue = numberValue
+        if numberValue < minValue:
+            minValue = numberValue
+    return maxValue - minValue
+
+
+class Tests():
+    import pytest
+
+    def test_example_1(x):
+        assert solve(get_example_input('day02')) == 18
+
+    def test_solve(x):
+        assert solve(get_input('day02'))
+
+if __name__ == "__main__":
+    print(solve(get_input('day02')))
