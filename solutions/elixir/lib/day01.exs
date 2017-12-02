944cc7794a0f904b982077c0413163a2c713b44c
Nathan Chere <git@nathanchere.com.au>
2017-12-02 15:29:52 +0100

    Misc elixir test structuring

--- solutions/elixir/lib/day01.exs
+++ solutions/elixir/lib/day01.exs
@@ -3,26 +3,23 @@
 
 defmodule Day01A do
 
-  def solve(input) do
+  def solve(_) do
     "OK"
   end
+
 end
 
 ExUnit.start
 defmodule Day01ATests do
   use ExUnit.Case
-
   import Common
 
-  test "Example" do
-    assert Day01A.solve("1122") == 3
-  end
-
-  test "Solution" do
-    input=Common.get_input("day01.input")
-    Day01A.solve(input)
-  end
+  test "Example1", do: assert Day01A.solve("1122") == 3
+  test "Example2", do: assert Day01A.solve("1111") == 4
+  test "Example3", do: assert Day01A.solve("1234") == 0
+  test "Example4", do: assert Day01A.solve("91212129") == 9
+  test "Solution", do: get_input("day01.input") |> Day01A.solve()
 end
-#
+
 # Day01A.solve("day01.input")
 # |> IO.puts
